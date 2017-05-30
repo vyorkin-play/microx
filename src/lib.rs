fac30f60361096f4c76586c9761d472611fe4521
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-05-30 21:26:39 +0300

    Add initial vga_buffer abstraction module

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,27 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate volatile;
+
+mod vga_buffer;
+
+use vga_buffer::*;
 
 #[no_mangle]
-pub extern fn rust_main() {
-    let x = ["hello", "world", "!"];
-    let y = x;
-    let test = (0..3).flat_map(|x| 0..x).zip(0..);
-
+pub extern "C" fn rust_main() {
     // ATTENTION: we have a very small stack and no guard page
+    print_something();
 
-    let hello = b"Hello World!";
-    let color_byte = 0x1f; // white foreground, blue background
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    // write `Hello World!` to the center of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-
-    loop{}
+    loop {}
 }
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern "C" fn panic_fmt() -> ! {
+    loop {}
+}
